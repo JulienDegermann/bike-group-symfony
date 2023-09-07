99038ff56df8017df0e574552d13cfa765b991c8
JulienDegermann <degermann.julien@gmail.com>
2023-09-07 15:16:05 +0200

    adjust config

--- src/Controller/DimancheController.php
+++ src/Controller/DimancheController.php
@@ -6,13 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class HomeController extends AbstractController
+class DimancheController extends AbstractController
 {
-    #[Route('/', name: 'app_home')]
+    #[Route('/dimanche', name: 'app_dimanche')]
     public function index(): Response
     {
-        return $this->render('home/home.html.twig', [
-            'controller_name' => 'HomeController',
+        return $this->render('dimanche/dimanche.html.twig', [
+            'controller_name' => 'DimancheController',
         ]);
     }
 }
